94bb497ff726c242817680bf369de7bfc353c3c3
Tal Aviram <me@talaviram.com>
2025-08-09 14:21:31 +0200

    win build script to build_win folder.

--- build_win.bat
+++ build_win.bat
@@ -1,6 +1,6 @@
 @echo off 
 REM This assumes you have an AAX codesign tools. to build without, simply don't pass the AAX_SIGN_ID
-cmake -B ./build -DAAX_SIGN_ID=7915a408860606418b22b8942bb2f3c0f5fe3eee`
-cmake --build ./build --config RelWithDebInfo
-cd .\build
+cmake -B ./build_win -DAAX_SIGN_ID=7915a408860606418b22b8942bb2f3c0f5fe3eee`
+cmake --build ./build_win --config RelWithDebInfo
+cd .\build_win
 cpack -C RelWithDebInfo
