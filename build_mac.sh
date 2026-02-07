f47e79ead0e03e6816b883837a1391086eb4e983
Tal Aviram <me@talaviram.com>
2026-02-07 11:57:32 +0100

    build_mac script

---
+++ build_mac.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cmake -B ./build_mac -G "Xcode" -DAAX_SIGN_ID="Tal Aviram (7P7G2A8U54)" -DCMAKE_XCODE_ATTRIBUTE_DEVELOPMENT_TEAM=7P7G2A8U54
+cmake --build ./build_mac --config RelWithDebInfo
+cd ./build_mac
+cpack -C RelWithDebInfo
